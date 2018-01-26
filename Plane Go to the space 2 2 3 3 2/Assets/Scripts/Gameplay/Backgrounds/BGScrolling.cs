8fbc8af7030d76dcfc58cf40fb459975e44206cc
Hiep <luvluv2410@gmail.com>
2018-01-26 18:10:11 +0700

    Update BG gameplay
    Update BG gameplay

---
+++ Plane Go to the space 2 2 3 3 2/Assets/Scripts/Gameplay/Backgrounds/BGScrolling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScrolling : MonoBehaviour {
+
+	public float scrollSpeed;
+
+	private Material mat;
+
+	private Vector2 offset = Vector2.zero;
+
+	// Use this for initialization
+	void Awake () {
+		mat = GetComponent <Renderer> ().material;
+	}
+
+	void Start ()
+	{
+		offset = mat.GetTextureOffset ("_MainTex"); 
+	}
+	// Update is called once per frame
+	void Update () {
+		offset.y += scrollSpeed * Time.deltaTime;
+		mat.SetTextureOffset ("_MainTex", offset);
+	}
+}
