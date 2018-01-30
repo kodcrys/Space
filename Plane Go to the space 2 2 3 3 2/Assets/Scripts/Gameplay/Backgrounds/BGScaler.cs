f600b186d3b976e25b619e927273afc19b92cefa
Hiep <luvluv2410@gmail.com>
2018-01-30 09:56:40 +0700

    Fix Bg and infinity fuel on endless mode

--- Plane Go to the space 2 2 3 3 2/Assets/Scripts/Gameplay/Backgrounds/BGScaler.cs
+++ Plane Go to the space 2 2 3 3 2/Assets/Scripts/Gameplay/Backgrounds/BGScaler.cs
@@ -8,11 +8,14 @@ public class BGScaler : MonoBehaviour {
 	// Use this for initialization
 	void Update () {
 		// Get the height resolution
-		var worldHeight = Camera.main.orthographicSize * 10;
+		var worldHeight = Camera.main.orthographicSize * 4;
 		// Get the width resolution
 		var worldWidth = worldHeight * Screen.width / Screen.height;
 
 		// Scale background fit with the resolution
 		transform.localScale = new Vector3 (worldWidth, worldHeight, 0f);
+		Vector3 temp = Camera.main.transform.position;
+		temp.z = 0f;
+		transform.position = temp;
 	}
 }
